4ed8caeafadec4c5c6d8fe0ac77392deee28e496
unknown <timurmaratovich@yandex.ru>
2023-03-28 16:03:14 +0300

    Gitignore

--- Assets/firstScripts/Move.cs
+++ Assets/firstScripts/Move.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public 
+    public CharacterController controller;
 
-    void Start()
-    {
-        
-    }
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
+
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    bool isGround;
 
     void Update()
     {
-        Input.KeyDown
+        isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+        
+        if (Input.GetButtonDown("Jump") && isGround)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
